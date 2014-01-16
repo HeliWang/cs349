bd3444f69b3a22f25af210704a241e1f7706a55a
Edward Yang <jh.edwardyang@gmail.com>
2014-01-16 10:16:13 -0500

    readme + a1 base

---
+++ a1/PaintEvent.cpp
@@ -0,0 +1,28 @@
+#include "PaintEvent.h"
+#include "XWindow.h"
+#include "Logging.h"
+
+namespace cs349
+{
+
+  PaintEvent::PaintEvent(XWindow* window, const Rectangle & damagedArea) : ComponentEvent(window)
+  {
+    this->damagedArea = damagedArea;
+  }
+
+  PaintEvent::~PaintEvent()
+  {
+    ; // no-op
+  }
+
+  void PaintEvent::DispatchEvent()
+  {
+    LOG_TODO << "TODO CS349: Implement PaintEvent::DispatchEvent()";
+// TODO CS349
+  }
+
+  Rectangle PaintEvent::GetDamagedArea() const
+  {
+    return this->damagedArea;
+  }
+}
